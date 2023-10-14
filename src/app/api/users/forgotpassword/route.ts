b79dd7b32211cd0f454db54b2ec8d6024911eb9c
tripathykrishna <tripathyswoyamprava@gmail.com>
2023-10-14 21:39:54 +0530

    krishna

---
+++ src/app/api/users/forgotpassword/route.ts
@@ -0,0 +1,10 @@
+import {connect} from "@/dbConfig/dbConfig";
+import User from "@/models/userModel";
+import { NextRequest, NextResponse } from "next/server";
+import bcryptjs from "bcryptjs";
+import { sendEmail } from "@/helpers/mailer";
+
+connect
+
+
+// export
