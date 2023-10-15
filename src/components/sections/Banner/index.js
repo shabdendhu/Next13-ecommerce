4f81921e5d48cc431feffe26e71bf5fef982d819
shabdendhu.tripathy <shabdendhus@lecturenotes.in>
2023-10-15 20:24:18 +0530

    dn

---
+++ src/components/sections/Banner/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./Banner.module.scss";
+
+const Banner = () => {
+  return <div className={styles.component}>Banner</div>;
+};
+
+export default Banner;
--- src/components/sections/Header/index.js
+++ src/components/sections/Header/index.js
@@ -4,19 +4,29 @@ import SearchIcon from "@mui/icons-material/Search";
 
 const Header = () => {
   return (
-    <div className={styles.headerContainer}>
-      <div className={styles.content}>
-        <div className={styles.logoContainer}>LOGO</div>
-        <div className={styles.searchBarContainer}>
-          <input
-            placeholder="Search Products Here...."
-            className={styles.searchBbar}
-          />
-          <span className={styles.searchButton}>
-            <SearchIcon className={styles.searchIcon} />
-          </span>
+    <div className={styles.component}>
+      <div className={styles.headerContainer}>
+        <div className={styles.content}>
+          <div className={styles.logoContainer}>LOGO</div>
+          <div className={styles.searchBarContainer}>
+            <input
+              placeholder="Search Products Here...."
+              className={styles.searchBbar}
+            />
+            <span className={styles.searchButton}>
+              <SearchIcon className={styles.searchIcon} />
+            </span>
+          </div>
+          <button className={styles.loginButton}>Login</button>
         </div>
-        <button className={styles.loginButton}>Login</button>
+      </div>
+      <div className={styles.navBar}>
+        <div className={styles.navContent}>
+          <div className={styles.navItem}>Category 1</div>
+          <div className={styles.navItem}>Category 2</div>
+          <div className={styles.navItem}>Category 3</div>
+          <div className={styles.navItem}>Category 4</div>
+        </div>
       </div>
     </div>
   );
