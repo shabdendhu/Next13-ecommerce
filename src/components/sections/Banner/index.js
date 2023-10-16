3655d6a26f8ed33ba3662ccf1f10b5c71a42b01b
shabdendhu.tripathy <shabdendhus@lecturenotes.in>
2023-10-16 10:41:26 +0530

    s

--- src/components/sections/Banner/index.js
+++ src/components/sections/Banner/index.js
@@ -1,20 +1,33 @@
 import React from "react";
 import styles from "./Banner.module.scss";
-import ArrowBackIosNewOutlinedIcon from '@mui/icons-material/ArrowBackIosNewOutlined';
-import ArrowForwardIosOutlinedIcon from '@mui/icons-material/ArrowForwardIosOutlined';
+import ArrowBackIosNewOutlinedIcon from "@mui/icons-material/ArrowBackIosNewOutlined";
+import ArrowForwardIosOutlinedIcon from "@mui/icons-material/ArrowForwardIosOutlined";
 
 const Banner = () => {
-  return <div className={styles.component}>
-    
-    <ArrowBackIosNewOutlinedIcon/>
-    
-    <img  src="https://img.crofarm.com/images/promotions/f6ceff3a1f04.png" alt=""/>
-    <img  src="https://img.crofarm.com/images/promotions/f6ceff3a1f04.png" alt=""/>
-    <img  src="https://img.crofarm.com/images/promotions/f6ceff3a1f04.png" alt=""/>
-    <img  src="https://img.crofarm.com/images/promotions/f6ceff3a1f04.png" alt=""/>
-    
-    <ArrowForwardIosOutlinedIcon/>
-    </div>;
+  return (
+    <div className={styles.component}>
+      <ArrowBackIosNewOutlinedIcon style={{ color: "#FFFFFF" }} />
+      <div className={styles.bannerContainer}>
+        <img
+          src="https://img.crofarm.com/images/promotions/f6ceff3a1f04.png"
+          alt=""
+        />
+        <img
+          src="https://img.crofarm.com/images/promotions/f6ceff3a1f04.png"
+          alt=""
+        />
+        <img
+          src="https://img.crofarm.com/images/promotions/f6ceff3a1f04.png"
+          alt=""
+        />
+        <img
+          src="https://img.crofarm.com/images/promotions/f6ceff3a1f04.png"
+          alt=""
+        />
+      </div>
+      <ArrowForwardIosOutlinedIcon style={{ color: "#FFFFFF" }} />
+    </div>
+  );
 };
- 
+
 export default Banner;
