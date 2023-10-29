786809f848abf1c9b70d906c50da2e529c49e7ae
tripathykrishna <tripathyswoyamprava@gmail.com>
2023-10-29 18:01:10 +0530

    f

---
+++ src/app/(admin)/product-manager/page.tsx
@@ -0,0 +1,8 @@
+"use client";
+import React, { useEffect } from "react";
+import ProductManager from "@/components/sections/ProductManager";
+
+export default function LoginPage() {
+  return <ProductManager/> ; 
+}
+
