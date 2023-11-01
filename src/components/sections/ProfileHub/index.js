62b9915ba379e42ad989fef7e62c605e25fd2392
tripathykrishna <tripathyswoyamprava@gmail.com>
2023-11-01 09:31:16 +0530

    xhjf

--- src/components/sections/ProfileHub/index.js
+++ src/components/sections/ProfileHub/index.js
@@ -1,7 +1,9 @@
 import React from "react";
 import styles from "./profile.module.scss";
 import AccountCircleRoundedIcon from "@mui/icons-material/AccountCircleRounded";
-
+import IosShareIcon from "@mui/icons-material/IosShare";
+import ChevronRightOutlinedIcon from "@mui/icons-material/ChevronRightOutlined";
+import InventoryIcon from '@mui/icons-material/Inventory';
 const Profile = () => {
   return (
     <div className={styles.profilebgcontainer}>
@@ -15,15 +17,49 @@ const Profile = () => {
       >
         <div className={styles.leftcontainer}>
           <div className={styles.heading}>
-            <AccountCircleRoundedIcon style={{fontSize:'50px',color:'green',marginRight:'15px'}}/>
-            <span>
+            <AccountCircleRoundedIcon
+              style={{ fontSize: "50px", color: "green", marginRight: "15px" }}
+            />
+            <span className={styles.headingText}>
               <p>Hello</p>
-              <b style={{fontSize:'25px'}}>swoyamprava</b>
+              <b style={{ fontSize: "25px" }}>swoyamprava</b>
             </span>
           </div>
-          <div style={{ border: "1px solid red", height: "100% " }}>
-            <div>hfjhj</div>
-            <div>hygghj</div>
+          <div
+            style={{
+              display: "flex",
+              border: "1px solid red",
+              height: "100% ",
+              backgroundColor: "white",
+              flexDirection: "column",
+              alignItems: "center",
+            }}
+          >
+            <div
+              style={{
+                display: "flex",
+                border: "1px solid blue",
+                width: "100%",
+              }}
+            >
+              <InventoryIcon
+                style={{
+                  margin: "0px 10px",
+                  fontSize: "25px", color: "green",
+                }}
+              />
+              <div
+                style={{
+                  fontSize: "20px",
+                  flex: 1,
+                  border: "1px solid red",
+                  fontWeight: 600,
+                  color:'#878787'
+                }}
+              >
+                MY ORDERS
+              </div>
+            </div>
           </div>
         </div>
         <br />
