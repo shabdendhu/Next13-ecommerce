f40b993c1481e0c404186401bd80fe9b43af6f96
shabdendhu.tripathy <shabdendhus@lecturenotes.in>
2023-11-20 10:00:06 +0530

    d

--- src/components/sections/ProductCard/index.js
+++ src/components/sections/ProductCard/index.js
@@ -5,8 +5,55 @@ import Productdetails from "@/components/sections/Productdetails";
 import cx from "classnames";
 import AddButton from "@/components/base/AddButton";
 import { useRouter } from "next/navigation";
-
-const ProductCard = ({ data, className, ...props }) => {
+const dummydata = {
+  ratings: {
+    average: 4.5,
+    count: 10,
+  },
+  dimensions: {
+    length: 10,
+    width: 5,
+    height: 2,
+  },
+  shipping_info: {
+    free_shipping: true,
+    estimated_delivery: "2-3 business days",
+  },
+  _id: "655733b294651a3e957bb3ea",
+  name: "Lembu Product",
+  description: "This is a sample product description.",
+  price: 29.99,
+  category_ids: [],
+  brand: "Sample Brand",
+  stock_quantity: 100,
+  images: [
+    "https://www.tastycircle.com/wp-content/uploads/2020/08/Garlic-Pickle-Veluthulli-Achar-500x500.jpg",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ27k4rXyjQz2SMl-PNzcdJYEcsYUGyvP-CbCGfnMxnf4VxfdELB0W6N-g5kosBgX3biPE&usqp=CAU",
+  ],
+  attributes: [
+    {
+      name: "Color",
+      value: "Red",
+      _id: "655733b294651a3e957bb3eb",
+    },
+    {
+      name: "Size",
+      value: "Medium",
+      _id: "655733b294651a3e957bb3ec",
+    },
+  ],
+  reviews: [],
+  sku: "SAMPLE_SKU",
+  weight: 1.5,
+  tags: ["tag1", "tag2"],
+  availability: "In Stock",
+  related_products: [],
+  created_at: "2023-11-17T09:34:42.137Z",
+  updated_at: "2023-11-17T09:34:42.137Z",
+  __v: 0,
+  discount: 10,
+};
+const ProductCard = ({ data = dummydata, className, ...props }) => {
   const router = useRouter();
   const handleRedirect = () => {
     router.push("/product-details/" + data._id);
