9984171dcc9452da3435c877047be78d90272e50
shabdendhu.tripathy <shabdendhus@lecturenotes.in>
2024-03-06 20:46:32 +0530

    d

--- src/components/base/ImageUpload/index.js
+++ src/components/base/ImageUpload/index.js
@@ -20,11 +20,11 @@ const ImageUpload = ({ images, setImages }) => {
 
       const res = await axios.post("/api/upload", data);
       // handle the error
-      const productImageUrl = `http://localhost:3000/api/upload?id=${res.data.data._id}`;
+      const productImageUrl = `https://acharpapad.in/api/upload?id=${res.data.data._id}`;
       if (res.status === 200) {
         setImages([...images, productImageUrl]);
       }
-      console.log(`http://localhost:3000/api/upload?id=${res.data.data._id}`);
+      console.log(productImageUrl);
       // if (!res.ok) throw new Error(await res.text());
     } catch (e) {
       // Handle errors here
